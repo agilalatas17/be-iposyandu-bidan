7308e5a088e04b5c9c37cc9df3274d360b3adb20
agilalatas <agilalatas17@gmail.com>
2024-09-29 01:16:51 +0700

    install cors

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-let indexRouter = require('./routes/index');
-let ibuHamilRouter = require('./routes/ibuHamil');
+const indexRouter = require('./routes/index');
+const ibuHamilRouter = require('./routes/ibuHamil');
 
-let app = express();
+const app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
