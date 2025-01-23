1149669c4c6abb7903eefc8a35703ba72e0837ac
agilalatas <agilalatas17@gmail.com>
2025-01-24 00:13:35 +0700

    membuat migrasi table refresh token

---
+++ users-service/db/models/RefreshTokenModel.js
@@ -0,0 +1,44 @@
+module.exports = (sequelize, DataTypes) => {
+  const RefreshTokenModel = sequelize.define(
+    'RefreshTokenModel',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      token: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      bidanId: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        field: 'bidan_id',
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: 'created_at',
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: 'updated_at',
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        field: 'deleted_at',
+      },
+    },
+    {
+      tableName: 'refresh_token',
+      modelName: 'RefreshTokenModel',
+      paranoid: true,
+    }
+  );
+
+  return RefreshTokenModel;
+};
