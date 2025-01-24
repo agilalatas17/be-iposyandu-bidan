24278392ba861d2692873638f18bff31746893ae
agilalatas <agilalatas17@gmail.com>
2025-01-24 18:40:11 +0700

    get Token API

--- users-service/routes/refreshTokens.js
+++ users-service/routes/refreshTokens.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const createRefreshToken = require('../controllers/refresh-token/create');
+const getToken = require('../controllers/refresh-token/getToken');
 
 router.post('/api/refresh-token', createRefreshToken);
+router.get('/api/get-token', getToken);
 
 module.exports = router;
