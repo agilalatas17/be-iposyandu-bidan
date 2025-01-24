b569d8edd04b63503d7fcd26c5ffb281ac3c634c
agilalatas <agilalatas17@gmail.com>
2025-01-24 22:43:43 +0700

    perubahan struktur folder

--- api-gateway/routes/ibuHamilRoute.js
+++ api-gateway/routes/ibuHamilRoute.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {
-  jumlahIbuHamil,
-  getAllIbuHamil,
-  getIbuHamilById,
-  createIbuHamil,
-  updateIbuHamil,
-  deleteIbuHamil,
-} = require('../controllers/ibuHamilController');
+
+const jumlahIbuHamil = require('../controllers/ibu-hamil/count');
+const getAllIbuHamil = require('../controllers/ibu-hamil/getAll');
+const getIbuHamilById = require('../controllers/ibu-hamil/getById');
+const createIbuHamil = require('../controllers/ibu-hamil/create');
+const updateIbuHamil = require('../controllers/ibu-hamil/update');
+const deleteIbuHamil = require('../controllers/ibu-hamil/destroy');
 
 router.get('/api/ibu-hamil/jumlah-data', jumlahIbuHamil);
 router.get('/api/ibu-hamil', getAllIbuHamil);
