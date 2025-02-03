963c2423b607353fb7ee4f308e68395136510ce8
agilalatas <agilalatas17@gmail.com>
2025-02-03 22:17:07 +0700

    benerin route api gateway ibu hamil

--- api-gateway/routes/ibuHamilRoute.js
+++ api-gateway/routes/ibuHamilRoute.js
@@ -9,11 +9,20 @@ const createIbuHamil = require('../controllers/ibu-hamil/create');
 const updateIbuHamil = require('../controllers/ibu-hamil/update');
 const deleteIbuHamil = require('../controllers/ibu-hamil/destroy');
 
-router.get('/api/ibu-hamil/jumlah-data', verifyToken, jumlahIbuHamil);
-router.get('/api/ibu-hamil', verifyToken, getAllIbuHamil);
-router.get('/api/ibu-hamil/:id', verifyToken, getIbuHamilById);
-router.post('/api/ibu-hamil/create', verifyToken, createIbuHamil);
-router.put('/api/ibu-hamil/:id', verifyToken, updateIbuHamil);
-router.delete('/api/ibu-hamil/:id', verifyToken, deleteIbuHamil);
+// With JWT
+// router.get('/api/ibu-hamil/jumlah-data', verifyToken, jumlahIbuHamil);
+// router.get('/api/ibu-hamil', verifyToken, getAllIbuHamil);
+// router.get('/api/ibu-hamil/:id', verifyToken, getIbuHamilById);
+// router.post('/api/ibu-hamil/create', verifyToken, createIbuHamil);
+// router.put('/api/ibu-hamil/:id', verifyToken, updateIbuHamil);
+// router.delete('/api/ibu-hamil/:id', verifyToken, deleteIbuHamil);
+
+// NON JWT
+router.get('/api/ibu-hamil/jumlah-data', jumlahIbuHamil);
+router.get('/api/ibu-hamil', getAllIbuHamil);
+router.get('/api/ibu-hamil/:id', getIbuHamilById);
+router.post('/api/ibu-hamil/create', createIbuHamil);
+router.put('/api/ibu-hamil/:id', updateIbuHamil);
+router.delete('/api/ibu-hamil/:id', deleteIbuHamil);
 
 module.exports = router;
