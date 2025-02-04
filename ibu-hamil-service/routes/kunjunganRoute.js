d94efa5584fed1cd19a8a6390902ec0e3065b83e
agilalatas <agilalatas17@gmail.com>
2025-02-05 00:39:34 +0700

    membuat API delete kunjungan

--- ibu-hamil-service/routes/kunjunganRoute.js
+++ ibu-hamil-service/routes/kunjunganRoute.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const createKunjungan = require('../controllers/kunjungan/create');
 const updateKunjungan = require('../controllers/kunjungan/update');
+const deleteKunjungan = require('../controllers/kunjungan/destroy');
 
 router.post('/api/ibu-hamil/kunjungan', createKunjungan);
 router.put('/api/ibu-hamil/kunjungan/:id', updateKunjungan);
+router.delete('/api/ibu-hamil/kunjungan/:id', deleteKunjungan);
 
 module.exports = router;
