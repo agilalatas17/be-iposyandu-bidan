360ed3bd60af6d347c8eba60bf92dc576d0d43fb
agilalatas <agilalatas17@gmail.com>
2025-02-05 00:06:53 +0700

    membuat API update kunjungan

--- ibu-hamil-service/routes/kunjunganRoute.js
+++ ibu-hamil-service/routes/kunjunganRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { createKunjungan } = require('../controllers/kunjungan/create');
+const createKunjungan = require('../controllers/kunjungan/create');
+const updateKunjungan = require('../controllers/kunjungan/update');
 
 router.post('/api/ibu-hamil/kunjungan', createKunjungan);
+router.put('/api/ibu-hamil/kunjungan/:id', updateKunjungan);
 
 module.exports = router;
