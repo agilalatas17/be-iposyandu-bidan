ac9bea985e042d5d2ca9777bd51dcd90cfb7df55
agilalatas <agilalatas17@gmail.com>
2025-02-05 23:37:49 +0700

    membuat API get all kunjungan

--- ibu-hamil-service/routes/kunjunganRoute.js
+++ ibu-hamil-service/routes/kunjunganRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const getAllKunjungan = require('../controllers/kunjungan/getAll.js');
 const createKunjungan = require('../controllers/kunjungan/create');
 const updateKunjungan = require('../controllers/kunjungan/update');
 const deleteKunjungan = require('../controllers/kunjungan/destroy');
 
+router.get('/api/ibu-hamil/:ibu_hamil_id/kunjungan', getAllKunjungan);
 router.post('/api/ibu-hamil/kunjungan', createKunjungan);
 router.put('/api/ibu-hamil/kunjungan/:id', updateKunjungan);
 router.delete('/api/ibu-hamil/kunjungan/:id', deleteKunjungan);
